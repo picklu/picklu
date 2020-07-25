1759cd566c68da086af9c3d69ef5e87b092d9e40
Subrata Sarker <subrata_sarker@yahoo.com>
2020-07-25 11:12:16 +0900

    Use components to form a basic layout of the site

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Footer = () => (
+    <header>
+        <span>Picklu</span>
+    </header>
+)
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
-
+import Layout from "../components/layout"
 import "../sass/index.scss"
 
 
-const IndexPage = () => {
-    return <h1>Hello, Gatsby!</h1>
-}
+const IndexPage = () => (
+    <Layout>
+        <h1>Hello, Gatsby!</h1>
+        <h3>Welcome to my new Gatsby site...</h3>
+    </Layout>
+)
 
 export default IndexPage
